3bebb2b4ad9a77eda59139af9c3fd35bd37b2e28
EliasSchlie <EliasSchlie@users.noreply.github.com>
2025-10-21 22:32:18 +0200

    Test auto-deployment

--- core/views.py
+++ core/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello! Elias's Django is live! 🚀")
+    return HttpResponse("Hello! Elias's Django is live! 🚀 Changes get updated automatically!")
