e3416cf7227425ae5d30e20e1fa47f032409a5b2
EliasSchlie <EliasSchlie@users.noreply.github.com>
2025-10-22 07:28:01 +0200

    trying auto deployment speed

--- core/views.py
+++ core/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello! Elias's Django is live! 🚀 Changes get updated automatically!")
+    return HttpResponse("Hello! Elias's Django is live! 🚀")
