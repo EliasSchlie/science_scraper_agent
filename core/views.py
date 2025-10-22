aaa6ec3647ebc17ac6026020ddb28a0d2cf513a2
EliasSchlie <EliasSchlie@users.noreply.github.com>
2025-10-22 07:40:31 +0200

    testing cicd pipeline

--- core/views.py
+++ core/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello! Elias's Django is live! 🚀")
+    return HttpResponse("Hello! Elias's Django is live! 🚀 And I can just push changes.")
